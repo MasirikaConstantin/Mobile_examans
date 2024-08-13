bc3b3c07dbe967259c4e32499450d3e366718a8f
Masirika COCO <elconstant73@gmail.com>
2024-08-13 18:54:55 +0100

    Suit Ajout d'autres Ménus2

--- app/src/main/java/com/example/mescode/VideosTest.java
+++ app/src/main/java/com/example/mescode/VideosTest.java
@@ -22,7 +22,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class VideosTest extends AppCompatActivity {
+public class VideosTest extends MainActivity {
     private ImageView mImageViewMonImage;
     private Button mButtonCapturerImage;
     private VideoView mVideoViewMaVideo;
